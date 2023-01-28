b1561c80e8374c20b7666570a263c33ae48021bf
Lidia D <lidia.khmylko@gmail.com>
2023-01-27 20:52:58 -0800

    Web version

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const subject = ['Grandma', 'Uncle Sam', 'Obama', 'Little girl', 'Pinnoccio', 'Peter Pan', 'Spiderman', 'Dart Wader', 'Santa Claus', 'Postman', 'The driver'];
+const subject = ['Grandma', 'Harry Potter', '', 'Little girl', 'Pinnoccio', 'Peter Pan', 'Spiderman', 'Dart Wader', 'Santa Claus', 'Postman', 'The driver'];
 
 const predicate = ['eats', 'hacks', 'bites', 'bakes', 'paints', 'cuts', 'mashes', 'mixes', 'tinkers', 'teaches', 'builds', 'throws'];
 
@@ -6,15 +6,23 @@ const adj = ['purple', 'fat', 'fluffy', 'wavy', 'soft', 'dirty', 'rugged', 'usel
 
 const object = ['trees', 'lemons', 'meatballs', 'streets', 'schools', 'fish', 'water', 'phones', 'frogs', 'lightsabers', 'hedgehogs'];
 
-const my_subject = subject[Math.floor(Math.random() * subject.length)];
+const randomSentence = () => {
 
-const my_predicate = predicate[Math.floor(Math.random() * predicate.length)];
+  const my_subject = subject[Math.floor(Math.random() * subject.length)];
 
-const my_adj = adj[Math.floor(Math.random() * adj.length)];
+  const my_predicate = predicate[Math.floor(Math.random() * predicate.length)];
 
-const my_object = object[Math.floor(Math.random() * object.length)];
+  const my_adj = adj[Math.floor(Math.random() * adj.length)];
 
-console.log(`${my_subject} ${my_predicate} ${my_adj} ${my_object}.`);
+  const my_object = object[Math.floor(Math.random() * object.length)];
+
+  const sentence = `${my_subject} ${my_predicate} ${my_adj} ${my_object}.`;
+
+  console.log(sentence);
+
+  document.getElementById('display').innerHTML = sentence;
+
+};
 
 console.log(`%c  
                    /\\_/\\   
